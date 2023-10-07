bdd3c138998407d4e4b9f63dbf9b7322296d8a66
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-10-07 17:08:25 +0700

    Feat: aded detail field design

---
+++ app/src/main/java/com/example/rumahraga/ui/fragments/field/FieldDetailFragment.java
@@ -0,0 +1,23 @@
+package com.example.rumahraga.ui.fragments.field;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.rumahraga.R;
+
+public class FieldDetailFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_field_detail, container, false);
+    }
+}
